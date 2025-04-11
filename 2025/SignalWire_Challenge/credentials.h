514fb01199c481b9a84ad2e2702c529e75bf911c
Len <len.pgh@gmail.com>
2025-04-11 09:58:29 -0400

    Create credentials.h

---
+++ 2025/SignalWire_Challenge/credentials.h
@@ -0,0 +1,10 @@
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+#define WIFI_SSID "your_wifi_ssid"
+#define WIFI_PASSWORD "your_wifi_password"
+#define SIGNALWIRE_SPACE "your_space"
+#define SIGNALWIRE_ACCOUNT_SID "your_account_sid"
+#define SIGNALWIRE_AUTH_TOKEN "your_auth_token"
+
+#endif
