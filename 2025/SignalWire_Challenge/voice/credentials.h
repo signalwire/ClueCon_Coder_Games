7b77867d178b61a38eb01b1f6bff0d6f25b979c0
Len <len.pgh@gmail.com>
2025-04-14 11:04:06 -0400

    Create credentials.h

---
+++ 2025/SignalWire_Challenge/voice/credentials.h
@@ -0,0 +1,19 @@
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+// WiFi credentials
+#define WIFI_SSID "your_wifi_ssid"
+#define WIFI_PASSWORD "your_wifi_password"
+
+// SignalWire credentials
+#define SIGNALWIRE_SPACE "your_space"
+#define SIGNALWIRE_ACCOUNT_SID "your_account_sid"
+#define SIGNALWIRE_AUTH_TOKEN "your_auth_token"
+
+// Callback URL for SignalWire (ngrok URL) needed for led's to update
+#define CALLBACK_URL "https://YOUR.ngrok-free.app/callback"
+
+// Voice Call URL(Replace this with you SignalWire Space CXML(LAML bin)
+#define CALL_URL "https://len.signalwire.com/laml-bins/26d681d5-0a07-41d1-bb11-cb57976fe82c"
+
+#endif
