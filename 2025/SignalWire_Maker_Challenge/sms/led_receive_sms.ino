b44eeadad93bcdd608a085dec33ea802ba1e4eb4
Len <Len-PGH@users.noreply.github.com>
2025-05-05 15:20:11 -0400

    Maker Challenge

--- 2025/SignalWire_Maker_Challenge/sms/led_receive_sms.ino
+++ 2025/SignalWire_Maker_Challenge/sms/led_receive_sms.ino
@@ -33,7 +33,7 @@ String getHTML() {
   <!DOCTYPE html>
   <html>
   <head>
-    <title>Coder Games - SignalWire Challenge - 2025 ESP32 SMS LED Toggle Monitor</title>
+    <title>Coder Games - SignalWire Maker Challenge - 2025 ESP32 SMS LED Toggle Monitor</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
@@ -85,7 +85,7 @@ String getHTML() {
   </head>
   <body>
     <div class="container-fluid">
-      <h1 class="text-center text-white mb-4">Coder Games - SignalWire Challenge 2025 - ESP32 SMS LED Toggle Monitor</h1>
+      <h1 class="text-center text-white mb-4">Coder Games - SignalWire Maker Challenge 2025 - ESP32 SMS LED Toggle Monitor</h1>
       <div class="row">
         <div class="col-lg-3 col-md-4 col-sm-12 filter-section">
           <div class="card">
